3531fc964ae4c5051e639990dd3e651b0f79b3ee
ozanagma <ozanagma@gmail.com>
2020-06-07 19:43:17 +0300

    working code

--- client.c
+++ client.c
@@ -1,16 +1,22 @@
 #include <sys/msg.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h> 
 #include <pthread.h>
 #include <unistd.h>
 #include <errno.h>
-
+#include <time.h>
 
 #define COUNTRY_CNT 195
 #define MESSAGE_SIZE 1024
+#define ORDER_REPEAT 100
+#define MAX_ORDER_CNT 100000
+#define MAX_SLEEP_TIME_MS 1000
+
+typedef enum{
+ MANUAL,
+ AUTOMATIC
+}E_MODE;
 
 typedef enum{
   ORDER_CONTINUES,
@@ -18,71 +24,102 @@ typedef enum{
 }E_COMMAND;
 
 typedef struct{
-  int country_code;
+  long mes_type;
+  char country_name[10];
   E_COMMAND command;
   int order_count;
-}S_MESSAGE_CONTENT;
+}S_MESSAGE;
 
-typedef struct{
-  long mes_type;
-  S_MESSAGE_CONTENT content;
-}S_MESSAGE_BUF;
+void* send_order(S_MESSAGE* param);
 
-void* send_order(void* param);
-
+int msg_id;
 
 int main(int argc, char* argv[])
 {
-  printf("Client is started.\n");	
-  int msg_id;
-  key_t key;
-  S_MESSAGE_BUF send_message;
-
-  
-  send_message.mes_type 			= 1;
-  send_message.content.country_code 		= 0;
-  send_message.content.command 		= ORDER_CONTINUES;
-  send_message.content.order_count    	= 10;
-
-
-  key = 2234;
- 
-  msg_id = msgget(key, 0666);
- if(msg_id < 0){
-   printf("msgget error.");
-   printf("%s\n", strerror(errno));
-   exit(1);
- }
-  printf("IPC is get. Message id: %d \n", msg_id);
-  
-  while(1){
-    sleep(1);
-    char a = getchar();
-    if(a == 'a'){
-        if(fork() == 0){
-          execl(".client", "selam", NULL);
-          //execl("/usr/bin/gnome-terminal", "gnome-terminal", "-q", "-e", "./client", "turkey",(char*)0);
-        }
-        else
-        {
-          wait(NULL);
-        }
-    } 
+  if(argc != 2){
+	printf("Expected one(1) argument.\n");  
+	exit(0);
   }
   
+  char* p;
+  E_MODE mode = strtol(argv[1], &p, 10);
+  srand(time(0));
+  
+  if(mode != 0 && mode != 1){
+  	printf("0 for Manual Mode or 1 for Automatic Mode.\n");
+  	exit(0);
+  }
+  
+  char* cntr_name = argv[0];
+  int cntr_name_len = strlen(cntr_name);
+  char cntr_name_cln[cntr_name_len - 2];
+  memcpy(cntr_name_cln, cntr_name + 2, cntr_name_len -2);
+  printf("Welcome %s\n", cntr_name_cln);
+
+  key_t key = 2234;
+  msg_id = msgget(key, 0666);
+  if(msg_id < 0){
+   printf("%s\n", strerror(errno));
+   exit(0);
+ }
+
+  S_MESSAGE send_message;
+  memcpy(send_message.country_name, cntr_name_cln, strlen(cntr_name_cln));
+  send_message.mes_type = 1;
+
+  int current_order = 0;
+  int total_order = 0;
+  
+  switch(mode){
+	case MANUAL:	
+	   printf("You are in manual mode.\n");
+	   printf("To order vaccine please enter order count.\n");
+	   while(1){
+	      scanf("%d", &current_order);
+	      if(current_order != 0){
+   	         total_order += current_order;
+   	         send_message.command = ORDER_CONTINUES;
+   	         send_message.order_count = current_order;
+   	         send_order(&send_message);
+   	         printf("Current order: %d ", current_order);
+   	         printf("Total order: %d\n", total_order);
+   	         printf("To continue order please enter new order count. " );
+   	         printf("To stop order please enter \"0\".\n" );
+	      }	      
+   	      else{
+   	         send_message.command = ORDER_DONE;
+   	         send_message.order_count = current_order;
+   	         send_order(&send_message);
+   	         printf("Total order: %d Goodbye!\n", total_order);
+   	         exit(0);
+   	      }
+   	   }
+	break;
+	case AUTOMATIC:
+	   printf("You are in automatic mode.\n");
+	   int sleep_time_ms;
+	   for(int i = 0; i < ORDER_REPEAT; i++){
+	      current_order = rand() % MAX_ORDER_CNT + 1;
+  	      total_order += current_order;
+   	      send_message.command = ORDER_CONTINUES;
+   	      send_message.order_count = current_order;
+   	      send_order(&send_message);
+	      printf("Current order: %d ", current_order);
+   	      printf("Total order: %d\n", total_order);
+   	      sleep_time_ms = rand() % MAX_SLEEP_TIME_MS + 1; 
+   	      usleep(sleep_time_ms);
+	  }
+	  printf("Total order: %d Goodbye!", total_order);		
+	break; 
+  }
+
  return 0;
 }
-/*
-void* send_order(void* param)
-{
-  
-  for(int i = 0;i < 100;i++){
-    if(i == 10)   send_message.content.command 		= ORDER_DONE;
-    sleep(1);
-    int err;
-    err = msgsnd(msg_id, (void*)&send_message, MESSAGE_SIZE, 0);
-    if(err < 0){
-       printf("%s\n", strerror(errno));
-    }
 
-}*/
+void* send_order(S_MESSAGE* message)
+{
+   
+   int err = msgsnd(msg_id, (void*)message, sizeof(S_MESSAGE)- sizeof(long), 0);
+   if(err < 0)
+      printf("%s\n", strerror(errno)); 
+}
--- server.c
+++ server.c
@@ -6,10 +6,81 @@
 #include <pthread.h>
 #include <errno.h>
 
+typedef unsigned char bool;
+
+#define false 0
+#define true 1
 #define COUNTRY_CNT 195
 #define MESSAGE_SIZE 1024
 #define READ_END 0
 #define WRITE_END 1
+
+// ****** HASH FUNCTION IMPLEMENTATION  ******* //
+
+struct S_COUNTRY{
+   struct S_COUNTRY* next;
+   char* name;
+   int order_cnt;
+   bool is_connection_open;
+};
+
+struct S_COUNTRY* CountryOrderCount[COUNTRY_CNT];
+
+unsigned int hash(char* s)
+{
+   unsigned hashval;
+   
+   for(hashval = 0; *s != '\0'; s++)
+     hashval = *s + 31 * hashval;
+ 
+   return hashval % COUNTRY_CNT;
+}
+
+struct S_COUNTRY* lookup(char *s)
+{
+   struct S_COUNTRY* np;
+   
+   for(np = CountryOrderCount[ hash(s)]; np != NULL; np = np->next)
+      if(strcmp(s, np->name) == 0)
+         return np;
+   
+   return NULL;
+}
+
+struct S_COUNTRY* install(char* name)
+{
+   struct S_COUNTRY* np;
+   struct S_COUNTRY* temp;
+   unsigned int hashval;
+
+   
+   if((np = lookup(name)) == NULL){ // country not exist in hashtable
+      np = (struct S_COUNTRY*) malloc(sizeof(*np));
+      hashval = hash(name);
+      if(CountryOrderCount[hashval] == NULL)  
+         CountryOrderCount[hashval] = np;
+      else{
+      temp = CountryOrderCount[hashval];
+      while(temp->next != NULL)
+         temp = temp->next;
+      temp->next = np;
+      }
+      np->next = NULL;
+      np->name = name;
+      np->order_cnt = 0;
+      np->is_connection_open = false;
+      return np;
+   }
+   else{
+      return np;
+   }   
+
+}
+
+
+// ****** END OF HASH FUNCTION IMPLEMENTATION  ******* //
+
+
 
 typedef enum{
   ORDER_CONTINUES,
@@ -17,54 +88,54 @@ typedef enum{
 }E_COMMAND;
 
 typedef struct{
-  int country_code;
+  long mes_type;
+  char country_name[10];
   E_COMMAND command;
   int order_count;
-}S_MESSAGE_CONTENT;
-
-typedef struct{
-  long mes_type;
-  S_MESSAGE_CONTENT content;
-}S_MESSAGE_BUF;
+}S_MESSAGE;
 
 
-int CountryOrderCount[COUNTRY_CNT];
 pthread_t ThreadIDs[COUNTRY_CNT];
 pthread_attr_t ThreadAtts[COUNTRY_CNT];
-int PipeFileDes[2];
+int PipeFileDes[COUNTRY_CNT][2];
 
 void* recive_order(void* param);
+void clear_msg_buffer(int msg_id);
 
 int main()
 {
- printf("Server is started.\n");	
-
-
- S_MESSAGE_BUF receive_message;
-
+ S_MESSAGE receive_message;
  
  key_t key  = 2234;
- int msg_id = msgget(key, 0666 | IPC_CREAT);
+ int msg_id = msgget(key, 0666 | IPC_CREAT);  //new message queue is created.
+ 
  if(msg_id < 0){
    printf("msgget error.");
    printf("%s\n", strerror(errno));
    exit(1);
  }
- printf("IPC is created. Message id: %d \n", msg_id);
- if(pipe(PipeFileDes) == -1)
-   printf("Pipe failed.");
+ 
+  clear_msg_buffer(msg_id); // it erases data in the buffer just in case client sent message before server runs.
+
+ for(int i = 0; i < COUNTRY_CNT; i++){ // All countries pipe's are created.
+    if(pipe(PipeFileDes[i]) == -1)
+      printf("%dth pipe failed.", i);    
+ }
 
  while(1){
-   if(msgrcv(msg_id, (void*)&receive_message, MESSAGE_SIZE, 1, 0) > 0){
-     if(CountryOrderCount[receive_message.content.country_code] == 0){
-       printf("New country %d\n", receive_message.content.country_code);
-       pthread_attr_init(&ThreadAtts[receive_message.content.country_code]);
-       pthread_create(&ThreadIDs[receive_message.content.country_code], &ThreadAtts[receive_message.content.country_code], recive_order, NULL);
 
-
-     }
-     write(PipeFileDes[WRITE_END], (void*)&receive_message.content, sizeof(S_MESSAGE_CONTENT));   
-   }
+      if(msgrcv(msg_id, (void*)&receive_message, sizeof(S_MESSAGE)- sizeof(long), 1, 0) > 0) {
+         int hash_value =  hash(receive_message.country_name);
+         struct S_COUNTRY* country = install(receive_message.country_name); // put country in the hashtable.
+         if(!country->is_connection_open){ // it handles 2 cases: 1) Current country's first order. 2) Current country close its connection and re-opens it.
+            country->is_connection_open = true;
+            pthread_attr_init(&ThreadAtts[hash_value]); // new thread.
+            pthread_create(&ThreadIDs[hash_value], 
+       	      &ThreadAtts[hash_value], 
+       	      recive_order, (void*)&hash_value);
+        }
+        write(PipeFileDes[hash_value][WRITE_END], (void*)&receive_message, sizeof(S_MESSAGE));  //send data to related thread.
+    }
  }
   
   return 0;
@@ -73,22 +144,36 @@ int main()
 
 void* recive_order(void* param)
 {
-  S_MESSAGE_CONTENT message_content;
+int hash_value = *(int*)param;
+  S_MESSAGE message;
+
   while(1){
-    read(PipeFileDes[READ_END], (void*)&message_content, sizeof(S_MESSAGE_CONTENT));
-    switch(message_content.command)
+    read(PipeFileDes[hash_value][READ_END], (void*)&message, sizeof(S_MESSAGE));
+    switch(message.command)
     {
     	case ORDER_CONTINUES:
-    	  CountryOrderCount[message_content.country_code] += message_content.order_count;
-    	  printf("Order received.");
-    	  printf("Country code: %d", message_content.country_code);
-    	  printf("Order count: %d", message_content.order_count);
-    	  printf("Total order count: %d\n", CountryOrderCount[message_content.country_code]);
-     	  break;
+    	   CountryOrderCount[hash_value]->order_cnt += message.order_count;
+    	   printf("%-10s -- ", message.country_name);
+    	   printf("Order Count: %-6d -- ", message.order_count);
+    	   printf("Total Order Count: %d\n", lookup(message.country_name)->order_cnt);
+     	   break;
      	case ORDER_DONE:
-     	  pthread_exit(NULL);
+         lookup(message.country_name)->is_connection_open = false;
+         printf("%-10s Communication end -- Total Order Count: %d\n", message.country_name, lookup(message.country_name)->order_cnt);
+     	   pthread_exit(NULL);
      	  break;
     }
   }
+}
+
+void clear_msg_buffer(int msg_id)
+{
+	S_MESSAGE message;
+	ssize_t ret;
+	do{
+	   ret = msgrcv(msg_id, (void*)&message , sizeof(S_MESSAGE) - sizeof(long), 1, IPC_NOWAIT);
+	}while(errno != ENOMSG);	
+	
+	printf("Message buffer is cleaned.\n");
 }
 
